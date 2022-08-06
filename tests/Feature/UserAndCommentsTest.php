5752c6f3dcaf9f2211abbe31d2dd9e6df4a55b43
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-08-07 00:44:54 +0100

    creating readme file for documentation

--- tests/Feature/UserAndCommentsTest.php
+++ tests/Feature/UserAndCommentsTest.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class UserAndCommentsTest extends TestCase
@@ -23,7 +24,7 @@ class UserAndCommentsTest extends TestCase
         $user = User::factory()->create();
         $comment = $this->faker->text(60);
 
-        $this->actingAs($user)->json('POST', url('/api/user/'.$user->id.'/comment'), [
+        $this->actingAs($user)->json('POST', route('user.comment.store', $user->id), [
             'user_id' => $user->id,
             'password' => '00000000',
             'comment' => $comment,
